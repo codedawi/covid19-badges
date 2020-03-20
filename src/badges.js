91291a4aaad37b040910c0ef06abd8a35706b00b
codedawi <brett@codedawi.com>
2020-03-20 00:06:24 -0400

    :boom: Service fetching covid19 impact data.

---
+++ src/badges.js
@@ -0,0 +1,12 @@
+const { badgen } = require("badgen")
+
+// const svgString = badgen({
+//   label: "npm",     // <Text>
+//   labelColor: "ADF", // <Color RGB> or <Color Name> (default: "555")
+//   status: "v1.2.3", // <Text>, required
+//   color: "blue",    // <Color RGB> or <Color Name> (default: "blue")
+//   style: "flat",    // "flat" or "classic" (default: "classic")
+// })
+
+
+
---
+++ src/services.js
@@ -0,0 +1,113 @@
+const axios = require('axios').default;
+
+const url = "https://coronavirus-tracker-api.herokuapp.com/v2/locations";
+
+/**
+ * Interface COVID-19 Cases Statistics
+ * 
+ * @typedef {Object} Covid19Impact
+ * @property {Number} confirmed - number of confirmed cases of COVID-19.
+ * @property {Number} deaths - number of confirmed deaths of COVID-19.
+ * @property {Number} recovered - number of confirmed recovers of COVID-19.
+ */
+
+
+ /**
+  * Get latest cases, deaths, and recovers globally of COVID-19
+  */
+async function getLatestGlobally() {
+
+    const { data } = await axios.get(`${url}/v2/latest`);
+
+    return data;
+}
+/**
+ * Get latest cases, deaths, and recovers globally of COVID-19
+ * for specific country.
+ * 
+ * @param {String} country = Alpha-2 Country Code (https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2)
+ * @returns {Object} 
+ */
+async function getLatestByCountry(country) {
+
+    const params = {
+        "country_code": country,
+        "timelines": 0,
+    };
+
+    const { data } = await axios.get(`${url}/v2/locations`, { params });
+
+    const latestZero = {
+        confirmed: 0,
+        deaths: 0,
+        recovered: 0,
+    };
+
+    const latest = data.locations.reduce(
+        (total, province) => {
+            return {
+                confirmed: total.confirmed + province.latest.confirmed,
+                deaths: total.deaths + province.latest.deaths,
+                recovered: total.recovered + province.latest.recovered,
+            }
+        },
+        latestZero,
+    )
+
+    return { latest };
+}
+
+module.exports = { getLatestGlobally, getLatestByCountry, }
+
+/**
+ *****************
+ WORK IN PROGRESS
+*****************
+    async function getChangeGlobally(since) {
+        // TODO: work in progress
+        if (!since) since = new Date(Date.now() - 1000 * 60 * 60 * 24);
+
+        // ignore time
+        since.setHours(0,0,0,0)
+
+        const params = {
+            "timelines": 1,
+        };
+
+        const { data } = await axios.get(`${url}/v2/locations`, { params });
+
+        const changeZero = {
+            confirmed: 0,
+            deaths: 0,
+            recovered: 0,
+        };
+
+
+        const change = data.locations.reduce(
+            (diff, province) => {
+                
+                let total = 0;
+
+                Object.entries(province.timelines)
+                    .forEach(([d, count]) => {
+                        // 2020-01-22T00:00:00Z  
+                        const jsd = Date.parse(d.replace("Z", ""))
+                        // ignore time
+                        jsd.setHours(0, 0, 0, 0)
+                        
+                        if (jsd >= since) {
+                            total += count
+                        }
+                    });
+                return diff - total;
+            },
+            changeZero,
+        )
+
+        return change;
+
+    }
+*/
+
+
+
